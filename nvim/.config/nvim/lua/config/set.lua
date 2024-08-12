6523589b1150506a7b9b02d5a1931a3fa59e2af8
weiseng18 <tws.hci@gmail.com>
2024-08-12 21:03:08 +0800

    nvim: other sane defaults

--- nvim/.config/nvim/lua/config/set.lua
+++ nvim/.config/nvim/lua/config/set.lua
@@ -4,3 +4,11 @@ v.tabstop = 2 -- # of spaces a <Tab> counts for
 v.softtabstop = 2 -- # of spaces a <Tab> is when editing
 v.expandtab = true -- space instead tab when pressing <Tab>
 v.shiftwidth = 2 -- # of spaces to use for each >> and <<
+v.relativenumber = true -- line numbers relative to cursor line
+v.number = true -- set current line number as actual
+v.swapfile = false -- disables swap file
+v.backup = false -- disables backup
+v.mouse = nil
+v.colorcolumn = '81'
+
+vim.g.mapleader = ' ' -- set leader to <space>
