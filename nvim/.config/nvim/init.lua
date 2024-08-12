a715910b711878b134a255722c4f781cc600dffd
weiseng18 <tws.hci@gmail.com>
2024-08-12 21:49:02 +0800

    nvim: init lazy and install color scheme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,12 @@
+require("config.lazy").setup {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  }
+}
+
 require("config.set")
+
+vim.cmd[[colorscheme tokyonight-moon]]
