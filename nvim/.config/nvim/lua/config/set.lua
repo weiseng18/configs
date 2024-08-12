2e38123d973130014e2dcd76b45243e5fdd3a5d9
weiseng18 <tws.hci@gmail.com>
2024-08-12 20:44:04 +0800

    nvim: sane indent defaults

---
+++ nvim/.config/nvim/lua/config/set.lua
@@ -0,0 +1,6 @@
+local v = vim.opt
+
+v.tabstop = 2 -- # of spaces a <Tab> counts for
+v.softtabstop = 2 -- # of spaces a <Tab> is when editing
+v.expandtab = true -- space instead tab when pressing <Tab>
+v.shiftwidth = 2 -- # of spaces to use for each >> and <<
