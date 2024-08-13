c8f7bb3319814e75a9681eed6f5f7e51ed3418cf
weiseng18 <tws.hci@gmail.com>
2024-08-13 23:49:19 +0800

    nvim: custom telescope select

--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -1,5 +1,24 @@
 local builtin = require("telescope.builtin")
+local actions = require("telescope.actions")
 local keymap = vim.keymap.set
+
+-- telescope developers prefer working with buffers
+--
+-- this function replaces the tab if it is the only blank tab,
+-- otherwise it opens in a new tab
+local function select_tab_hack(opts)
+  local tabpages = vim.api.nvim_list_tabpages()
+  local current_tab = vim.api.nvim_get_current_tabpage()
+  local buf_list = vim.fn.tabpagebuflist(current_tab)
+  local buf_name = vim.fn.bufname(buf_list[1])
+
+  if #tabpages == 1 and buf_name == '' then
+    return actions.select_default(opts)
+  else
+    return actions.select_tab(opts)
+  end
+end
+
 
 -- find files, including hidden ones
 keymap('n', '<leader>ff', function()
@@ -17,14 +36,13 @@ keymap('n', '<leader>fw', function()
   })
 end, {})
 
-local actions = require("telescope.actions")
 require('telescope').setup {
   defaults = {
     -- custom keybindings for actions
     mappings = {
       i = {
-        ['<CR>'] = actions.select_tab,
         ['<Esc>'] = actions.close,
+        ['<CR>'] = select_tab_hack,
       },
     },
   },
