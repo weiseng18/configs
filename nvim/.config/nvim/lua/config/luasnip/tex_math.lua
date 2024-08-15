647abd778ba9f7d0fdcb5938115281811431e769
weiseng18 <tws.hci@gmail.com>
2024-08-15 22:02:19 +0800

    nvim: more math snippets

--- nvim/.config/nvim/lua/config/luasnip/tex_math.lua
+++ nvim/.config/nvim/lua/config/luasnip/tex_math.lua
@@ -2,7 +2,6 @@ local ls = require("luasnip")
 local s = ls.snippet
 local i = ls.insert_node
 local fmta = require("luasnip.extras.fmt").fmta
-local rep = require("luasnip.extras").rep
 
 local in_math = function()
   return vim.fn['vimtex#syntax#in_mathzone']() == 1
@@ -12,5 +11,35 @@ ls.add_snippets("tex", {
   s({trig="=>", snippetType="autosnippet"},
     fmta([[\implies]], {}),
     { condition = in_math }
-  )
+  ),
+
+  s({trig="inv", snippetType="autosnippet"},
+    fmta([[^{-1}]], {}),
+    { condition = in_math }
+  ),
+
+  s({trig=">=", snippetType="autosnippet"},
+    fmta([[\geq]], {}),
+    { condition = in_math }
+  ),
+
+  s({trig="<=", snippetType="autosnippet"},
+    fmta([[\leq]], {}),
+    { condition = in_math }
+  ),
+
+  s({trig="->", snippetType="autosnippet"},
+    fmta([[\rightarrow]], {}),
+    { condition = in_math }
+  ),
+
+  s({trig="<-", snippetType="autosnippet"},
+    fmta([[\leftarrow]], {}),
+    { condition = in_math }
+  ),
+
+  s({trig="//", snippetType="autosnippet"},
+    fmta([[\\frac{<>}{<>}]], { i(1), i(2) }),
+    { condition = in_math }
+  ),
 })
