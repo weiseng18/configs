85fc3dcacc4730cbb7bed71395598f6aa1d7e31e
weiseng18 <tws.hci@gmail.com>
2024-08-15 22:24:38 +0800

    nvim: basic vimtex setup

--- nvim/.config/nvim/lua/config/misc.lua
+++ nvim/.config/nvim/lua/config/misc.lua
@@ -1,1 +1,9 @@
 vim.cmd('cnoreabbrev te tabedit')
+
+-- vimtex
+vim.g.vimtex_view_method = 'skim'
+vim.g.vimtex_compiler_method = 'latexmk'
+vim.g.vimtex_quickfix_ignore_filters = {
+  'badness 10000',
+}
+
