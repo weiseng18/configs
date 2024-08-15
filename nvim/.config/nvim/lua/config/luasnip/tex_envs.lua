563188b0d5d797aa2134da9ad67924cf5036157a
weiseng18 <tws.hci@gmail.com>
2024-08-15 21:34:05 +0800

    nvim: remove unneeded params

--- nvim/.config/nvim/lua/config/luasnip/tex_envs.lua
+++ nvim/.config/nvim/lua/config/luasnip/tex_envs.lua
@@ -1,6 +1,5 @@
 local ls = require("luasnip")
 local s = ls.snippet
-local t = ls.text_node
 local i = ls.insert_node
 local fmta = require("luasnip.extras.fmt").fmta
 
@@ -8,8 +7,7 @@ ls.add_snippets("all", {
   s("ssq",
     fmta(
       [[\section*{Question <>}]],
-      { i(1) },
-      { delimeters = "<>" }
+      { i(1) }
     )
   ),
 
@@ -20,11 +18,9 @@ ls.add_snippets("all", {
           <>
         \end{align*}
       ]],
-      { i(1) },
-      { delimeters = "<>" }
+      { i(1) }
     )
   ),
-})
 
   s("beql",
     fmta(
