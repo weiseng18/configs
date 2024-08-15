9fc46a0e9a82c328f27ec17788d34231e985a149
weiseng18 <tws.hci@gmail.com>
2024-08-15 18:24:15 +0800

    nvim: refactor and fix tex

--- nvim/.config/nvim/lua/config/luasnip/tex_header.lua
+++ nvim/.config/nvim/lua/config/luasnip/tex_header.lua
@@ -2,29 +2,34 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local fmta = require("luasnip.extras.fmt").fmta
+
+ls.filetype_extend("tex", { "plaintex" })
 
 ls.add_snippets("plaintex", {
-	s("init", {
-		t({
-			"\\documentclass[a4paper]{article}",
-			"\\usepackage[margin=1.2in]{geometry}",
-			"\\usepackage{amsmath, amssymb}",
-			"\\usepackage{xcolor}",
-			"",
-			"\\usepackage{graphicx}",
-			"\\graphicspath{ {./images/} }",
-			"",
-			"% ref with brackets",
-			"\\let\\oldref\\ref",
-			"\\renewcommand{\\ref}[1]{(\\oldref{#1})}",
-			"",
-			"\\begin{document}",
-			"",
-		}),
-		i(1),
-		t({
-			"",
-			"\\end{document}",
-		}),
-	}),
+  s("init",
+    fmta(
+      [[
+        \documentclass[a4paper]{article}
+        \usepackage[margin=1.2in]{geometry}
+        \usepackage{amsmath, amssymb}
+        \usepackage{xcolor}
+
+        \usepackage{graphicx}
+        \graphicspath{ {./images/} }
+
+        % ref with brackets
+        \let\oldref\ref
+        \renewcommand{\ref}[1]{(\oldref{#1})}
+
+        \begin{document}
+
+        <>
+
+        \end{document}
+      ]],
+      { i(1) }
+    )
+  )
 })
+
