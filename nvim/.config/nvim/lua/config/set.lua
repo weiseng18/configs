f33206eae5eea3dc1f5dde6d373fbbe7e1f17ae4
weiseng18 <tws.hci@gmail.com>
2024-09-01 14:36:12 +0800

    nvim: 4 space indentation for CS2103T Java

--- nvim/.config/nvim/lua/config/set.lua
+++ nvim/.config/nvim/lua/config/set.lua
@@ -11,3 +11,9 @@ v.backup = false -- disables backup
 v.mouse = nil
 
 vim.g.mapleader = ' ' -- set leader to <space>
+
+-- 4 space indentation for CS2103T Java
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "java",
+  command = "setlocal shiftwidth=4 tabstop=4",
+})
